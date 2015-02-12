36ef1e32f4ae45d857a47eef1896325fb12035ce
Richard Todd <waywardcoder@users.noreply.github.com>
2015-02-12 13:07:44 -0600

    shortened many words
    shortened many words

--- sierpinski/sierp.fs
+++ sierpinski/sierp.fs
@@ -1,14 +1,16 @@
 64 constant SIZE   CHAR * constant '*'
-create cur-line bl c, SIZE chars allot bl c, 
-create new-line SIZE chars allot
+: char-array create does> swap chars + ;
+char-array cur[]  bl c, SIZE chars allot bl c, 
+char-array new[]  SIZE chars allot            
 
-new-line SIZE blank   '*' new-line SIZE 2/ chars + c!  \ init
+0 new[] SIZE blank   '*' SIZE 2/ new[] c!  \ initialize
 
-: .new ( -- ) new-line SIZE type cr ;
-: cur=*? ( i -- f ) chars cur-line + c@ '*' = ;
+: .new ( -- ) 0 new[] SIZE type cr ;
+: cur=*? ( i -- f ) cur[] c@   '*' = ;
 : parents ( i -- f1 f2 ) dup cur=*?  swap 2 + cur=*?  ;
-: new! ( i f -- ) [ '*' bl - ] LITERAL and bl + swap chars new-line + c! ;
-: sierp-line ( -- ) SIZE 0 DO  i dup parents xor new!  LOOP ;
-: new>cur ( -- ) new-line cur-line 1 chars + SIZE chars move ;
+: >new[] ( i f -- ) [ '*' bl - ] LITERAL and bl + swap new[] c! ;
+: sierp-line ( -- ) SIZE 0 DO  i dup parents xor >new[]  LOOP ;
+: new>cur ( -- ) 0 new[]   1 cur[]   SIZE chars move ;
 : main ( -- ) SIZE 2/ 0 DO .new new>cur sierp-line LOOP ;
+
 main bye
