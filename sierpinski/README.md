e086bb8abca62e2dc2ad991a26027ae519bd77f1
Richard Todd <waywardcoder@users.noreply.github.com>
2015-06-09 17:42:21 -0500

    fixed up some formatting.
    I like code spaced out these days.  like  f(  a, b );

--- sierpinski/README.md
+++ sierpinski/README.md
@@ -6,6 +6,9 @@ was written in response to [yet another "challenge"][2] on `comp.lang.forth`.
 _Edit: 2015-05-18_: I did a version in scala while learning the language, `sierp.scala`,
                     and added an implementation in prolog for good measure. 
 
+_Edit: 2015-06-09_: Added a c++ program, which came out more like the forth version
+                    than I expected.
+
 <pre><code>
                                 *
                                * *
--- sierpinski/sierp.cpp
+++ sierpinski/sierp.cpp
@@ -4,11 +4,11 @@
 // can take any forward iterator as long as it's giving out chars...
 template< typename It, typename OIt >
 static void next_sierp( It first, It last, OIt out ) {
-  char prev {' '};
-  char cur  {*first++};
+  char prev { ' '      };
+  char cur  { *first++ };
   while( first != last ) {
-    char next {*first++};
-    *out++ = ((prev ^ next) == 0) ? ' ' : '*'; 
+    char next { *first++ };
+    *out++ = ( ( prev ^ next ) == 0 ) ? ' ' : '*' ; 
     prev = cur;
     cur = next; 
   }
@@ -18,8 +18,9 @@ static void next_sierp( It first, It last, OIt out ) {
 static const size_t HALF_SZ { 32 } ;
 
 int main( int argc, char **argv ) {
-  std::string sierp = std::string( HALF_SZ, ' ') + std::string(1,'*') +
-                      std::string( HALF_SZ, ' '); 
+  std::string sierp { std::string( HALF_SZ, ' ' ) + 
+                      std::string(       1, '*' ) +
+                      std::string( HALF_SZ, ' ' ) }; 
   for( int idx = 0; idx < HALF_SZ; ++idx ) {
     std::cout << sierp << std::endl;
     next_sierp( begin( sierp ), end( sierp ), begin( sierp ) );
