61bfc3ed4987d1620499db7ab9a298ca8fd5a668
Richard Todd <waywardcoder@users.noreply.github.com>
2015-08-10 14:50:11 -0500

    added scala pascal triangle

---
+++ print_pascaltri/p_pasc.scala
@@ -0,0 +1,22 @@
+object PascalTri {
+  
+  def draw( height : Int ) : Unit = {
+
+     // inner func, draw a single line of the triangle..
+     def draw1( width: Int ) : Unit = {
+        var cur = 1
+        for ( loc <- 0 to width ) {
+           print(s"$cur ")
+           cur =  (width-loc)*cur / (loc+1)
+        }     
+        println()
+     }
+
+     // draw all the lines
+     (0 until height) foreach draw1
+  }
+
+  def main( argv : Array[String] )  = {
+     draw( 16 )
+  }
+}
