0bca57a5f5af2bd226652ea3452032b0c46c1f35
Richard Todd <waywardcoder@users.noreply.github.com>
2015-08-11 10:25:49 -0500

    adjusted scala hanoi program

--- print_hanoi/print_hanoi.scala
+++ print_hanoi/print_hanoi.scala
@@ -1,34 +1,29 @@
 object PrintHanoi {
 
-  def draw( d1 : Array[Int], 
-            d2 : Array[Int], 
-            d3 : Array[Int], 
-            spacing : Int ) : Unit = {
+  def draw( d1: Array[Int], 
+            d2: Array[Int], 
+            d3: Array[Int], 
+            spacing: Int ): Unit = {
 
-    val disks = d1.length + d2.length + d3.length
-    def formatted_size( d : Int ) = d*2+1
+    val height = d1.length + d2.length + d3.length
+    def formatted_size( d: Int ) = d*2+1
     val colsize = formatted_size(d1.max max d2.max max d3.max) 
+    val spacingstr = " "*spacing
 
-    def printRow(d : Int) = { 
-       val dsz = formatted_size(d)
+    def drawOne( column: Array[Int], row: Int ) = { 
+       val diskidx = row - (height - column.length)
+       val dsz = if ( diskidx >= 0 ) formatted_size( column(diskidx) ) else 1
        val spaces = " "*((colsize - dsz)/2)
-       var chars = (if(dsz == 1) { "|" } else { "-" })*dsz
-       print(s"$spaces$chars$spaces${" "*spacing}")
+       var chars = (if(dsz == 1) "|"  else "-")*dsz
+       print(s"$spaces$chars$spaces$spacingstr")
     }
 
-    def diskAtRow( dlist: Array[Int], row : Int ) = {
-       val firstIndex = disks -  dlist.length
-       if ( row >= firstIndex ) { dlist(row - firstIndex) } else { 0 }
-    }
-
-    for ( row <- 0 until disks ) {
-       printRow( diskAtRow( d1, row ) )
-       printRow( diskAtRow( d2, row ) )
-       printRow( diskAtRow( d3, row ) )
+    for ( row <- 0 until height ) {
+       drawOne( d1, row ) ; drawOne( d2, row ) ; drawOne( d3, row )
        println
     } 
   }  
 
-  def main( argv : Array[String] ) = 
+  def main( argv: Array[String] ) = 
      draw( Array(4,5,6,7), Array(2,3), Array(1), 1)
 }
