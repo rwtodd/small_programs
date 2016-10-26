fd0737d8fc5ed8680f43a8698464eb3080a3a6ed
Richard Todd <rwtodd@users.noreply.github.com>
2016-10-26 16:59:52 -0500

    separated concerns, and generally over-engineered this tiny utility

--- ips_patch/ips.scala
+++ ips_patch/ips.scala
@@ -4,43 +4,84 @@ import java.io.RandomAccessFile
 import java.io.InputStream
 import java.io.FileInputStream
 import java.io.BufferedInputStream
+import java.nio.file.{Files,FileSystems,Path,StandardCopyOption}
 
-class IpsInput(fname : String) {
+// ============================================================================
+// There are 3 kinds of patches.. Byte Changes, RLE Patches, and the EOF patch.
+// Patches can apply themselves to random-access files, and have a useful
+// toSring().
+// ============================================================================
+sealed trait Patch {
+  def patch(tgt : RandomAccessFile) : Unit 
+}
+
+final class BytePatch(offs: Long, bs : Array[Byte]) extends Patch {
+  override def toString() : String = s"Patch of length ${bs.length}"
+  override def patch(tgt : RandomAccessFile) : Unit = {
+     tgt.seek(offs)
+     tgt.write(bs)
+  }
+} 
+
+final class RlePatch(offs: Long, len: Int, value: Byte) extends Patch {
+  override def toString() : String = s"RLE Patch of length ${len}, value ${value.toInt}"
+  override def patch(tgt : RandomAccessFile) : Unit = {
+     tgt.seek(offs)
+     val buf = new Array[Byte](len)
+     java.util.Arrays.fill(buf,value) 
+     tgt.write(buf)
+  }
+}
+
+object EOFPatch extends Patch {
+  val MARKER = ('E' << 16) + ('O' << 8) + 'F'
+  override def toString() : String = "End of Patches marker."
+  override def patch(tgt : RandomAccessFile) : Unit = { }
+}
+
+// ============================================================================
+// The IpsIterator iterates through an IPS file, producing Patches.
+// ============================================================================
+class IpsIterator(fname : String) extends Iterator[Patch] {
   private val src = new BufferedInputStream(new FileInputStream(fname))
   
   private val header = new String(readBuf(5), java.nio.charset.StandardCharsets.US_ASCII)
   if(!header.equals("PATCH")) throw new Exception("Not a valid IPS file.")
 
-  def read3() : Int = {
-     val b1 = src.read()
-     val b2 = src.read()
-     val b3 = src.read()
-     if ((b1 == -1) || (b2 == -1) || (b3 == -1)) throw new Exception("Unexpected EOF!")
+  private var isDone = false
+  override def hasNext() : Boolean = isDone
 
-     (b1 << 16) + (b2 << 8) + b3 
+  override def next() : Patch = {
+      val offs = read3()
+      if((offs == EOFPatch.MARKER) && atEOF) {
+           close()
+           isDone = true
+           return EOFPatch
+      } 
+
+      read2() match {
+          case 0 => val rleLen = read2() 
+                    val rleVal = read().toByte
+                    new RlePatch(offs.toLong, rleLen, rleVal) 
+          case x => new BytePatch(offs.toLong, readBuf(x))
+      } 
   }
 
-  def read2() : Int = {
-     val b1 = src.read()
-     val b2 = src.read()
-     if ((b1 == -1) || (b2 == -1)) throw new Exception("Unexpected EOF!")
-
-     (b1 << 8) + b2 
-  }
-
-  def read() : Byte = {
+  private def read() : Int = {
      val ans = src.read()
      if (ans == -1) throw new Exception("Unexpected EOF!")
-     ans.toByte
+     ans
   }
+  private def read2() : Int = (read() << 8) + read()
+  private def read3() : Int = (read2() << 8) + read() 
 
-  def readBuf(size : Int) : Array[Byte] = {
+  private def readBuf(size : Int) : Array[Byte] = {
      val ans = new Array[Byte](size)
      if( src.read(ans,0,size) != size ) throw new Exception("Read error!")
      ans
   }
 
-  def atEOF : Boolean = {
+  private def atEOF : Boolean = {
      src.mark(1)
      val ans = src.read() == -1 
      src.reset()
@@ -50,53 +91,25 @@ class IpsInput(fname : String) {
   def close() : Unit = src.close()
 }
 
+// ============================================================================
+// This is the "main" object, which parses the cmdline and runs the patch.
+// ============================================================================
 object Ips {
-
-  val EOF = ('E' << 16) + ('O' << 8) + 'F'
-
-  def patch(tgt : RandomAccessFile, src : IpsInput) : Unit = {
-      import java.util.Arrays
-
-      var count = 0
-
-      while(true) {
-         val offs = src.read3()
-         if((offs == EOF) && src.atEOF) {
-             println(s"EOF. ${count} total changes.")
-             return
-         } 
-
-         count += 1
-
-         val changeBuf = src.read2() match {
-                           case 0 => val rleLen = src.read2() 
-                                     val rleVal = src.read()
-                                     println(s"Change ${count} (offset ${offs}): RLE of ${rleVal.toInt}, length ${rleLen}")
-                                     val buf = new Array[Byte](rleLen)
-                                     Arrays.fill(buf,rleVal) 
-                                     buf
-
-                           case x => println(s"Change ${count} (offset ${offs}): Patch of length ${x}")
-                                     src.readBuf(x)
-                        } 
-         tgt.seek(offs.toLong)
-         tgt.write(changeBuf)
-      }
-
-  }
-
-  import java.nio.file.{Files,FileSystems,Path,StandardCopyOption}
 
   def main(args: Array[String]) : Unit =  args.toList match { 
      case ips :: orig :: newf :: Nil => 
         Files.copy(FileSystems.getDefault().getPath(orig),
                    FileSystems.getDefault().getPath(newf),
                    StandardCopyOption.REPLACE_EXISTING) 
-        val src = new IpsInput(ips)
+        val src = new IpsIterator(ips)
         val tgt = new RandomAccessFile(newf,"rw")
 
         try {
-           patch(tgt, src)
+            src.zipWithIndex
+               .foreach { case (p, idx) => 
+                               println(s"Change $idx: $p") 
+                               p.patch(tgt)
+                        } 
         } catch {
            case x : Exception => println(x.toString)
         } finally {
