da873be9c2d2490908dc85dbebe18522d52e8da3
Richard Todd <rwtodd@users.noreply.github.com>
2016-11-12 13:08:12 -0600

    added aspect ratio to calculation

--- ascii_pic/asciipic.scala
+++ ascii_pic/asciipic.scala
@@ -5,23 +5,33 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-object AsciiPic {
 
-  // load the given image, and scale it down to the given width
-  def scaleImage(fname: String, width: Int) : BufferedImage = {
-      val image = ImageIO.read(new File(fname))
-      val scaled = new BufferedImage(width, 
-                                     ((width.toDouble / image.getWidth) * 
-                                         image.getHeight).toInt,
-                                     BufferedImage.TYPE_INT_RGB)
+object Thumbnail {
+  private def scaleImage(orig: BufferedImage, width: Int, height: Int) : BufferedImage = {
+      val scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
       val gfx2d = scaled.createGraphics()
       gfx2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
                              RenderingHints.VALUE_INTERPOLATION_BICUBIC)
-      gfx2d.drawImage(image, 0, 0, scaled.getWidth(), scaled.getHeight, null)
+      gfx2d.drawImage(orig, 0, 0, width, height, null)
       gfx2d.dispose()                             
       scaled
   }
 
+  // load the given image, and progressively scale it down to the given width
+  def apply(fname: String, scaledWidth: Int, aspectRatio: Double) : BufferedImage = {
+      var image = ImageIO.read(new File(fname))
+      val scaledHeight = ((scaledWidth / aspectRatio / image.getWidth) * image.getHeight).toInt
+
+      while(scaledWidth < image.getWidth/2) {
+          image = scaleImage(image, image.getWidth/2, image.getHeight/2)
+      }
+
+      scaleImage(image, scaledWidth, scaledHeight) 
+  }
+
+}
+
+object AsciiPic {
 
   // determine the brightness (0.0 to 255.0) of a Color
   def brightness(c: Color)  = c.getRed * 0.2126 + c.getGreen * 0.7152 + c.getBlue * 0.0722 
@@ -44,8 +54,8 @@ object AsciiPic {
            
   def main(args: Array[String]) : Unit = 
     println( args.toList match { 
-              case fname :: Nil       =>  convertImage(scaleImage(fname, 72))
-              case fname :: sz :: Nil =>  convertImage(scaleImage(fname, Integer.valueOf(sz)))
+              case fname :: Nil       =>  convertImage(Thumbnail(fname, 72, 1.5))
+              case fname :: sz :: Nil =>  convertImage(Thumbnail(fname, Integer.valueOf(sz), 1.5)
               case _                  => "Usage: AsciiPic fname [width]"
               })
 }
