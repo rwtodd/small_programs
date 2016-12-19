28a39d913283e33b889fe8cc8e9cb6dd47304a27
Richard Todd <rwtodd@users.noreply.github.com>
2016-12-19 02:12:29 -0600

    first cut at a dotnet core version

--- ascii_pic/README.md
+++ ascii_pic/README.md
@@ -1,12 +1,11 @@
 # Ascii Pic
 
-A simple image-to-ascii converter in scala. 
+A simple image-to-ascii converter in scala and .NET CORE. 
 I had run across a [clojure version](http://www.bestinclass.dk/blog/tribute-steve-ballmer) 
 and wanted to see how a scala implementation would compare.  NB: I used a more sophisticated 
 brightness formula and I did not implement the HTML output option, so the two are not 100% equivalent.
 
-
-I have since made a Go version, [also on github](https://github.com/rwtodd/misc-go/tree/master/asciipic).
+I have also made a Go version, [also on github](https://github.com/rwtodd/misc-go/tree/master/asciipic).
 
 Here, I convert a picture of a soccer ball:
  
---
+++ ascii_pic/csharp_version/ascii_pic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using ImageSharp;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static double Brightness(Color c) =>  c.R * 0.2126 + c.G * 0.7152 + c.B * 0.0722;
+
+        private static char[] translation = new char[] { '#','A','@','%','$','+','=','*',':',',','.',' ' };
+
+        private static char ToChar(Color c) => translation[(int)(Brightness(c)*translation.Length/256.0)];
+
+        public static void Main(string[] args)
+        {
+            using (FileStream stream = File.OpenRead(args[0]))
+            {
+                Image image = new Image(stream);
+                var sb = new StringBuilder();
+                using(var pa = image.Resize(72, 0).Lock()) {
+                    for(int y = 0; y < pa.Height; y++) {
+                        for(int x = 0; x < pa.Width; x++) {
+                            sb.Append(ToChar(pa[x,y]));
+                        }
+                        sb.AppendLine();
+                    }
+                }
+                Console.WriteLine(sb.ToString());
+            }
+        }
+    }
+}
