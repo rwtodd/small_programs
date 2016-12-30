f963812fc6561d5ac8d1046792a799cfe202543c
Richard Todd <rwtodd@users.noreply.github.com>
2016-12-30 00:17:36 -0600

    added type alias to improve signatures

--- castgeo/scala/castGeo.scala
+++ castgeo/scala/castGeo.scala
@@ -4,23 +4,24 @@ object CastGeo {
   private val line = Array("*   *", "  *  ")
 
   private val rnd = new java.util.Random()
-  private def randomGeo() : Array[String] = Array.fill(4){ line(rnd.nextInt(2)) } 
+  private def randomFig() : Array[String] = Array.fill(4){ line(rnd.nextInt(2)) } 
 
-  // display an array of geomantic figures (a single row of them)
-  private def disp(initSp: Int, midSp: Int, figs : Array[Array[String]]) : Unit = 
+  type FigRow = Array[Array[String]]   
+
+  // display a row of geomantic figures, with initial and middle spaces
+  private def disp(initSp: Int, midSp: Int, figs : FigRow) : Unit = 
      figs.transpose.foreach { row =>
          print( row.mkString(" " * initSp, " " * midSp, "\n") )
      }
 
-  // combine adjacent geomantic figures from the given Array, returning
-  // an array of 1/2 size
-  private def combined(figs : Array[Array[String]]) : Array[Array[String]] =
+  // combine adjacent geomantic figures from a row, returning the resulting row
+  private def combined(figs : FigRow) : FigRow =
      figs.grouped(2).map{ pair => 
        (pair(0), pair(1)).zipped.map { (a,b) => line(if (a==b) 0 else 1) }
      }.toArray
 
   def main(args: Array[String]) : Unit = {
-     val moms      = Array.fill(4)(randomGeo)
+     val moms      = Array.fill(4)(randomFig)
      val daughters = moms.transpose
      val line1     = (moms ++ daughters).reverse
      val nieces    = combined(line1)
