4f3c0ef59685e7dc700570e30762951225bd7944
Richard Todd <rwtodd@users.noreply.github.com>
2017-01-06 23:54:45 -0600

    added haskell version

---
+++ castgeo/haskell/README.md
@@ -0,0 +1,12 @@
+# CastGeo in Haskell
+
+*Note:* it turns out modern versions of GHC ship **without** a 
+random number generator! I personally think that's crazy, but 
+it is easily remedied via `cabal`:
+
+    $ cabal install random
+
+Other than that issue, the haskell implementation went very 
+smoothly.  You can see this code is 
+concise and clear.
+
---
+++ castgeo/haskell/castgeo.hs
@@ -0,0 +1,33 @@
+-- cast a geomantic shield
+import Data.List(transpose, intersperse)
+import qualified System.Random as Rnd
+
+type Figure = [Bool]
+
+dispFigs :: Int -> Int -> [Figure] -> [String]  -- display a line of figures
+dispFigs sp1 spMid figs = map comb strs 
+  where strs     = map (map toStr) (transpose figs)
+        comb ln  = concat $ (spaces sp1) : (intersperse (spaces spMid) ln)
+        spaces n = take n (repeat ' ')
+        toStr bl = if bl then "  *  " else "*   *"
+
+combine :: [Figure] -> [Figure]  -- combine adjacent figures 
+combine [] = []
+combine (a : b : fs) = (mergeFigs a b) : (combine fs)
+ where mergeFigs = zipWith (\a b -> if a == b then False else True) 
+
+rfig :: IO Figure -- random geomantic figure
+rfig = do { gen <- Rnd.newStdGen ; return $ take 4 (Rnd.randoms gen) }
+
+displine l = (mapM_ putStrLn l) >> (putStrLn "")
+
+main = do
+  moms <- sequence $ take 4 (repeat rfig)
+  let line1 = reverse (moms ++ (transpose moms))
+  let nieces = combine line1 
+  let witnesses = combine nieces
+  let judge = combine witnesses
+  displine $ dispFigs 2 5 line1 
+  displine $ dispFigs 7 15 nieces
+  displine $ dispFigs 17 35 witnesses
+  displine $ dispFigs 37 0 judge
