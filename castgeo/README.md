a32be96020f035217767b4bf1b45df828e7cb9f0
Richard Todd <rwtodd@users.noreply.github.com>
2017-01-23 00:43:39 -0600

    added common lisp version of castgeo

--- castgeo/README.md
+++ castgeo/README.md
@@ -1,7 +1,7 @@
 # Geomantic Shield
 
 This little program produces and formats a geomantic shield.
-There are versions in Prolog, Scala, and Haskell. 
+There are versions in Prolog, Scala, Haskell, and common lisp. 
 
 ~~~~~
 $ ./castgeo 
---
+++ castgeo/lisp/castgeo.lisp
@@ -0,0 +1,52 @@
+; C A S T G E O
+; Create a geomantic shield
+
+; Create a random set of moms, along with
+; the transpositions as daughters.
+(defun moms-daughters () 
+  (let ((ans (make-array '(8 4))))
+    (dolist (i '(7 6 5 4) ans)
+      (dotimes (j 4)
+        (let ((v (random 2)))
+           (setf (aref ans i j)             v
+                 (aref ans (- 3 j) (- 7 i)) v))))))
+
+; Combine adjacent geomantic figures, returning
+; an array 1/2 the size of the input.
+(defun combine (src)
+  (let ((ans (make-array (list (/ (array-dimension src 0) 2)  
+                               4))))
+     (dotimes (i (array-dimension ans 0) ans) 
+        (dotimes (j 4)
+           (setf (aref ans i j) 
+                 (boole boole-xor (aref src (* i 2)       j)
+                                  (aref src (+ (* i 2) 1) j)))))))
+
+(defconstant +LINES+ (vector "*   *"  "  *  "))
+(defconstant +SPACES+ "                                         ")
+
+; Displays a line of geomantic figures FIGS, separated
+; by MSP spaces, with ISP initial spaces.
+(defun displine (isp msp figs)
+   (let ((ispaces (subseq +SPACES+ 0 isp))
+         (mspaces (subseq +SPACES+ 0 msp)))
+     (dotimes (j 4)
+       (princ ispaces)
+       (dotimes (i (array-dimension figs 0))
+          (princ (aref +LINES+ (aref figs i j)))
+          (princ mspaces))
+       (princ #\Newline))
+     (princ #\Newline)))
+
+; The main function.
+(defun castgeo () 
+  (let* ((line1     (moms-daughters))
+         (nieces    (combine line1))
+         (witnesses (combine nieces))
+         (judge     (combine witnesses)))
+    (displine 2   5 line1)
+    (displine 7  15 nieces)
+    (displine 17 35 witnesses)
+    (displine 37  0 judge)))
+
+(setf *random-state* (make-random-state t))
